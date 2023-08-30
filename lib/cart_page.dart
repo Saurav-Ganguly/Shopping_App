d05a6e476f1c04bf162b84b93adb463ed54a2fcd
Saurav Ganguly <hisauravganguly@gmail.com>
2023-08-30 13:51:32 +0530

    added the dialog box to remove the cart item

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -34,10 +34,53 @@ class CartPage extends StatelessWidget {
             subtitle: Text('Size: ${cartItem['size']}'),
             trailing: IconButton(
               onPressed: () {
-                Provider.of<CartProvider>(
-                  context,
-                  listen: false,
-                ).removeProduct(cartItem);
+                showDialog(
+                  //barrierDismissible:
+                  //  false, //does not allow to close the dialog box by clicking outside
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(
+                        'Delete Product',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      content: const Text(
+                        'Are you sure you want to remove the product from the cart?',
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Provider.of<CartProvider>(
+                              context,
+                              listen: false,
+                            ).removeProduct(cartItem);
+                            Navigator.of(context)
+                                .pop(); //removes the dialog box
+                          },
+                          child: const Text(
+                            'Yes',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            'No',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
               },
               icon: const Icon(Icons.delete),
               color: Colors.red,
